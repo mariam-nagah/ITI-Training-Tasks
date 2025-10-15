f9ab950f8ae45908fe214bd93594c6da8e97f52e
mariam-nagah <mariam000nagah@gmail.com>
2025-10-15 21:03:12 +0300

    message display

---
+++ JavaScript/Day-01/Task-01/main.js
@@ -0,0 +1,7 @@
+//--------task1--------
+
+ var message = prompt("Enter a message:");
+
+    for (var i = 1; i <= 6; i++) {
+      document.writeln("<h" + i + ">" + message + "</h" + i + ">");
+    }
